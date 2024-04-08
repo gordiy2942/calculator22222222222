7348b0d88197fdd139ca06607bb73178c8e7e7d4
Gordey <gordey.korchak@gmail.com>
2024-04-08 17:09:55 +0300

    first commit

---
+++ app/src/main/res/drawable/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.calculator22222222222
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator22222222222.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
+
+
+
+}
